7633ee6df0f2a262a3a660cb41daaa78f4a70285
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2018-02-23 13:21:36 +0300

    completed seventh task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  /*var zeroes = 0;
+  var poweredFive = 5; 
+  var i = 1;
+  
+  while(number/poweredFive > 1) {
+    zeroes += (number - number % poweredFive) / poweredFive;
+    poweredFive = Math.pow(5, ++i);
+  }
+  return zeroes;*/
+
+  var zeroes = 0;
+  var base = 10;
+  var primerArray = [];
+  var primer = 2;
+  var restBase = base;
+  var isPrimer = true;
+  while(primer <= base) {
+    primerArray.forEach(element => {
+        if((primer % element) == 0) {
+          isPrimer = false;
+        }
+    });
+    if(isPrimer && (restBase%primer == 0)) {
+      primerArray.push(primer);
+    }
+    isPrimer = true;
+    primer++;    
+  }
+  primer = primerArray.pop();
+  
+  while(number/primer > 1) {
+    zeroes += (number - number % primer) / primer;
+    number /= primer;
+  }
+  
+  return zeroes;
 }
